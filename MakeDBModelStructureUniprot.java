8103bb8b76d96b9e8c341e082df8ba0ff1a49053
Arnav Nagle <anagle@localhost.localdomain>
2023-09-21 11:23:35 -0700

    Remove non Java files

--- MakeDBModelStructureUniprot.java
+++ MakeDBModelStructureUniprot.java
@@ -29,32 +29,41 @@ public class MakeDBModelStructureUniprot {
 		int model_structure_id = model_paths_rs.getInt("id");
 		String cif_path = model_paths_rs.getString("cif_path");
 		Path cif_path_p = Paths.get(cif_path);
-		String[] sequenceEntryId = getSequenceEntryId(cif_path_p);
+		String[] sequenceEntryId = getCifInfo(cif_path_p);
 		String cifSeq = sequenceEntryId[0];
 		String unpEntryId = sequenceEntryId[1];
-	        System.out.println("Entry ID: " + unpEntryId);
-		System.out.println("cif seq: " + cifSeq);
-	        String[] DBseqUNPid = getDBseq(unpEntryId);
-		String DBseq = DBseqUNPid[0];
-		String UNPid = DBseqUNPid[1];
-	        System.out.println("astral seq: " + DBseq);
-		boolean match;
-		if (DBseq != null) {
-		match = DBseq.equals(cifSeq);
-		System.out.println("match: " + match);
-		} else {
-		match = false;
-		System.out.println("not found");
+		int unpStartZero = Integer.parseInt(sequenceEntryId[2]) - 1;
+		int unpEndZero = Integer.parseInt(sequenceEntryId[3]) - 1;
+	        // System.out.println("Entry ID: " + unpEntryId);
+		// System.out.println("cif seq: " + cifSeq);
+	        List<List<String>> DBseqs = getDBseqs(unpEntryId);
+		boolean match = false;
+		int numDBseq = 0;
+		while (numDBseq < DBseqs.get(0).size()) {
+		    String fullDBseq = DBseqs.get(0).get(numDBseq);
+		    String UNPid = DBseqs.get(1).get(numDBseq);
+	            // System.out.println("astral seq full: " + fullDBseq);
+		    // System.out.println("unpStartZero: " + unpStartZero);
+		    // System.out.println("unpEndZero: " + unpEndZero);
+
+		    if (unpEndZero < fullDBseq.length()) {
+		        String splicedDBseq = fullDBseq.substring(unpStartZero, unpEndZero + 1);
+	                // System.out.println("astral seq spliced: " + splicedDBseq);
+	       	        match = splicedDBseq.equals(cifSeq);
+			if (match) {
+			    // System.out.println("match: " + match);
+			    insertModelUNP.setInt(1, model_structure_id);
+			    insertModelUNP.setString(2, UNPid);
+			    insertModelUNP.setInt(3, unpStartZero);
+			    insertModelUNP.setInt(4, unpEndZero);
+			    // System.out.println(insertModelUNP);
+			    insertModelUNP.executeUpdate();
+			    break;
+			}
+		    }
+		    numDBseq++;
 		}
-		System.out.println("");
-		if (match) {
-			insertModelUNP.setInt(1, model_structure_id);
-			insertModelUNP.setString(2, UNPid);
-			insertModelUNP.setInt(3, 1);
-			insertModelUNP.setInt(4, 1);
-			// System.out.println(insertModelUNP);
-			// insertModelUNP.executeUpdate();
-		}
+		// System.out.println("");
 	    }
 	} catch (Exception e) {
             System.out.println("Exception: "+e.getMessage());
@@ -71,13 +80,16 @@ public class MakeDBModelStructureUniprot {
         return getAccession(p.getFileName().toString());
     }
 
-    private static String[] getSequenceEntryId(Path p) throws Exception {
+    private static String[] getCifInfo(Path p) throws Exception {
 	CifFile cifFile = CifIO.readFromPath(p);
 	MmCifFile mmCifFile = cifFile.as(StandardSchemata.MMCIF);
 	MmCifBlock data = mmCifFile.getFirstBlock();
         String seq = data.getEntityPoly().getPdbxSeqOneLetterCode().get(0).replaceAll("\n", "").toLowerCase();
-	String entryId = data.getStructRef().getDbCode().get(0); 
-	return new String[] {seq, entryId};
+	StructRef structRef = data.getStructRef();
+	String entryId = structRef.getDbCode().get(0); 
+	String unpStart = structRef.getPdbxAlignBegin().get(0);
+	String unpEnd = structRef.getPdbxAlignEnd().get(0);
+	return new String[] {seq, entryId, unpStart, unpEnd};
     }
 
     
@@ -90,16 +102,16 @@ public class MakeDBModelStructureUniprot {
     }
 
 
-    private static String[] getDBseq(String entryId) throws Exception {
-        String seq = null;	
-        String UNPid = null;	
-        String query = "select uniprot.id, astral_seq.seq from uniprot join uniprot_seq on uniprot.id = uniprot_seq.uniprot_id join astral_seq on astral_seq.id = uniprot_seq.seq_id where uniprot.long_id = '" + entryId + "' order by uniprot.seq_date desc limit 1;";	
+    private static List<List<String>> getDBseqs(String entryId) throws Exception {
+        List<String> seqs = new ArrayList<String>();	
+        List<String> UNPids = new ArrayList<String>();
+        String query = "select uniprot.id, astral_seq.seq from uniprot join uniprot_seq on uniprot.id = uniprot_seq.uniprot_id join astral_seq on astral_seq.id = uniprot_seq.seq_id where uniprot.long_id = '" + entryId + "' order by uniprot.seq_date desc;";	
 	ResultSet rs = doQuery(query);
-	if (rs.next()) {
-	    seq = rs.getString("seq");
-	    UNPid = rs.getString("id");
+	while (rs.next()) {
+	    seqs.add(rs.getString("seq"));
+	    UNPids.add(rs.getString("id"));
 	}
-	return new String[] {seq, UNPid};
+	return List.of(seqs, UNPids);
     }
 
     private static boolean isFrag(Path p) {
