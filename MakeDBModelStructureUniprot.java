a4dd9423c0bc34653fbe5b36c1d33d6831a042d6
Arnav Nagle <anagle@localhost.localdomain>
2023-09-21 16:59:02 -0700

    Rewrite database sequence information into a class

--- MakeDBModelStructureUniprot.java
+++ MakeDBModelStructureUniprot.java
@@ -19,52 +19,19 @@ import org.rcsb.cif.schema.*;
 
 public class MakeDBModelStructureUniprot {
 
-    public static void main(String[] args) {
-	try {
-            LocalSQL.connectRW();
-            String model_path_query = "select id, cif_path from model_structure;";
-	    ResultSet model_paths_rs = doQuery(model_path_query);
-	    PreparedStatement insertModelUNP = LocalSQL.prepareStatement("insert into model_structure_uniprot (model_structure_id, uniprot_id, uniprot_start, uniprot_end) values (?, ?, ?, ?);"); 
-	    while (model_paths_rs.next()) {
-		int model_structure_id = model_paths_rs.getInt("id");
-		String cif_path = model_paths_rs.getString("cif_path");
-		Path cif_path_p = Paths.get(cif_path);
-		CifInfo modelInfo = new CifInfo(cif_path_p);
-	        List<List<String>> DBseqs = getDBseqs(modelInfo.entryId);
-		boolean match = false;
-		int numDBseq = 0;
-		while (numDBseq < DBseqs.get(0).size()) {
-		    String fullDBseq = DBseqs.get(0).get(numDBseq);
-		    String UNPid = DBseqs.get(1).get(numDBseq);
+    /** Represents a sequence read from a cif file and its Uniprot reference.
+     */
+    private static class CifSeq {
 
-		    if (modelInfo.unpStart < fullDBseq.length()) {
-		        String splicedDBseq = fullDBseq.substring(modelInfo.unpStart, modelInfo.unpEnd + 1);
-	       	        match = splicedDBseq.equals(modelInfo.seq);
-			if (match) {
-			    insertModelUNP.setInt(1, model_structure_id);
-			    insertModelUNP.setString(2, UNPid);
-			    insertModelUNP.setInt(3, modelInfo.unpStart);
-			    insertModelUNP.setInt(4, modelInfo.unpEnd);
-			    insertModelUNP.executeUpdate();
-			    break;
-			}
-		    }
-		    numDBseq++;
-		}
-	    }
-	} catch (Exception e) {
-            System.out.println("Exception: "+e.getMessage());
-            e.printStackTrace();
-        }
-    }
+	String seq;
+	String entryId; // Uniprot entry ID
+	int unpStart; // zero-indexed, inclusive
+	int unpEnd; // zero-indexed, inclusive - TODO: check this
 
-    private static class CifInfo {
-        String seq;
-	String entryId;
-	int unpStart;
-	int unpEnd;
-
-	private CifInfo(Path p) throws Exception {
+        /** Read the relevant information from the cif file.
+	 * @param p: The Path where the cif file exists.
+         */
+	private CifSeq(Path p) throws Exception {
 	    CifFile cifFile = CifIO.readFromPath(p);
             MmCifFile mmCifFile = cifFile.as(StandardSchemata.MMCIF);
 	    MmCifBlock data = mmCifFile.getFirstBlock();
@@ -80,30 +47,65 @@ public class MakeDBModelStructureUniprot {
 	}
     }
 
-    private static List<List<String>> getDBseqs(String entryId) throws Exception {
-        List<String> seqs = new ArrayList<String>();	
-        List<String> UNPids = new ArrayList<String>();
-        String query = "select uniprot.id, astral_seq.seq from uniprot join uniprot_seq on uniprot.id = uniprot_seq.uniprot_id join astral_seq on astral_seq.id = uniprot_seq.seq_id where uniprot.long_id = '" + entryId + "' order by uniprot.seq_date desc;";	
-	ResultSet rs = doQuery(query);
-	while (rs.next()) {
-	    seqs.add(rs.getString("seq"));
-	    UNPids.add(rs.getString("id"));
+    /** Represents a sequence retrieved from the `uniprot` table and the associated primary key id.  */
+    private static class DBSeq {
+        String seq;
+	String UNPid; // id in uniprot table
+
+	private DBSeq(String seq, String UNPid) throws Exception {
+	    this.seq = seq;
+	    this.UNPid = UNPid;
 	}
-	return List.of(seqs, UNPids);
+
     }
 
+    /** Retrieve all sequences associated with a Uniprot entry ID.
+     * @param entryId: The Uniprot entry ID (field `long_id` in the uniprot table)
+     */
+    private static List<DBSeq> getDBseqs(String entryId) throws Exception {
+        List<DBSeq> DBseqs = new ArrayList<>();	
+        String query = "select uniprot.id, astral_seq.seq from uniprot join uniprot_seq on uniprot.id = uniprot_seq.uniprot_id join astral_seq on astral_seq.id = uniprot_seq.seq_id where uniprot.long_id = '" + entryId + "' order by uniprot.seq_date desc;";	
+        ResultSet rs = doQuery(query);
+        while (rs.next()) {
+            DBseqs.add(new DBSeq(rs.getString("seq"), rs.getString("id")));
+         }
+	return DBseqs;
+    }
     
     private static ResultSet doQuery(String query) throws Exception {
         Statement stmt = LocalSQL.createStatement();
 	return stmt.executeQuery(query);
     }
 
-    private static List<String> rsToList(ResultSet rs, String column) throws SQLException {
-        List<String> resultList = new ArrayList<String>();
-        while (rs.next()) {
-	    resultList.add(rs.getString(column));
-	}
-	return resultList;
+    public static void main(String[] args) {
+	try {
+            LocalSQL.connectRW();
+            String modelPathQuery = "select id, cif_path from model_structure;";
+	    ResultSet modelPathsRs = doQuery(modelPathQuery);
+	    PreparedStatement insertModelUNP = LocalSQL.prepareStatement("insert into model_structure_uniprot (model_structure_id, uniprot_id, uniprot_start, uniprot_end) values (?, ?, ?, ?);"); 
+	    while (modelPathsRs.next()) {
+		int modelStructureId = modelPathsRs.getInt("id");
+		Path cifPath = Paths.get(modelPathsRs.getString("cif_path"));
+		CifSeq modelSeq = new CifSeq(cifPath);
+	        List<DBSeq> DBseqs = getDBseqs(modelSeq.entryId);
+		for (DBSeq dbSeq : DBseqs) { 
+		    if (modelSeq.unpStart < dbSeq.seq.length()) {
+		        String splicedDBseq = dbSeq.seq.substring(modelSeq.unpStart, modelSeq.unpEnd + 1);
+			if (splicedDBseq.equals(modelSeq.seq)) {
+			    insertModelUNP.setInt(1, modelStructureId);
+			    insertModelUNP.setString(2, dbSeq.UNPid);
+			    insertModelUNP.setInt(3, modelSeq.unpStart);
+			    insertModelUNP.setInt(4, modelSeq.unpEnd);
+			    insertModelUNP.executeUpdate();
+			    break;
+			}
+		    }
+		}
+	    }
+	} catch (Exception e) {
+            System.out.println("Exception: "+e.getMessage());
+            e.printStackTrace();
+        }
     }
 
 }
