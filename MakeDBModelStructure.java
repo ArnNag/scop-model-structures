c0597402f3adfe93eecf8a4d37f7b38f268c5cae
Arnav Nagle <anagle@localhost.localdomain>
2023-09-21 17:57:01 -0700

    MakeDBModelStructure.java

--- MakeDBModelStructure.java
+++ MakeDBModelStructure.java
@@ -1,14 +1,8 @@
 import java.sql.*;
 import java.io.*;
 import java.util.*;
-import java.text.*;
 import java.nio.file.*;
 import java.util.stream.Stream;
-import org.strbio.IO;
-import org.strbio.io.*;
-import org.strbio.math.*;
-import org.strbio.mol.*;
-import org.strbio.util.*;
 import gov.lbl.scop.local.LocalSQL;
 
 public class MakeDBModelStructure {
@@ -16,8 +10,8 @@ public class MakeDBModelStructure {
     public static void main(String[] args) {
 	try {
             LocalSQL.connectRW();
-            PreparedStatement stmt1 = LocalSQL.prepareStatement("insert into model_structure (source_id, pdb_path, cif_path, header_date, file_date) values (1, ?, ?, 20220601, 20220505);");
-	    String af2_directory = "/mnt/net/ipa.jmcnet/data/h/anagle/af2_human_v3";
+            PreparedStatement stmt1 = LocalSQL.prepareStatement("insert into model_structure (source_id, pdb_path, cif_path) values (1, ?, ?);");
+	    String af2_directory = "/mnt/net/ipa.jmcnet/data/h/anagle/af2_human_v4";
 	    Stream<Path> files = Files.walk(Paths.get(af2_directory)).filter(x -> x.getFileName().toString().contains("pdb"));
 	    for (Path p : (Iterable<Path>)files::iterator) {
                 String pdb_path = p.toString();
@@ -28,7 +22,7 @@ public class MakeDBModelStructure {
 	    }
 	}
         catch (Exception e) {
-            System.out.println("Exception: "+e.getMessage());
+            System.out.println("Exception: " + e.getMessage());
             e.printStackTrace();
         }
     }
